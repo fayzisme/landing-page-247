eea590ffdaf9c345abca79e81ca7918bf4c423cb
Aneis Me <ammadfayz@gmail.com>
2023-08-11 01:10:05 +0700

    build

--- src/main.js
+++ src/main.js
@@ -3,18 +3,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
 import router from "./router/index"
-import VueYouTubeEmbed from 'vue-youtube-embed'
-// import firebase from 'firebase/app'
-// import 'firebase/firestore'
-
-Vue.use(VueYouTubeEmbed)
 Vue.config.productionTip = false
-
-// firebase.initializeApp({
-//  Configurações do seu app
-// })
-
-// export const db = firebase.firestore()
 
 new Vue({
   vuetify,
